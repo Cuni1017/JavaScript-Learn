d1dc5c8a9b0e13192ac2dc97ee14a54f2139be6f
Cuni <ahet1017@gmail.com>
2024-06-16 03:14:22 +0800

    ES6, 原型鏈

--- js1.js
+++ js1.js
@@ -2,6 +2,22 @@
 //獲取元素
 var oBox = document.getElementById("box")
 //點擊操作
-oBox.onclick = function(){
-    alert("red!")
+oBox.onclick = function () {
+  alert("red!")
 }
+
+
+// 創造階段
+var myName;
+function fn(myName) {
+  console.log(myName)
+}
+
+
+// 執行階段
+myName = "A"
+setTimeout(() => {
+  fn(myName);
+  myName = "B"
+}, 3000)
+myName = "C"
